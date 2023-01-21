e2b024804e1ccd582300dc6be10c69cd57992805
Brigitte <114067290+brishar0n@users.noreply.github.com>
2023-01-21 20:14:12 +0700

    Update mylib.c

--- mylib/mylib.c
+++ mylib/mylib.c
@@ -3,10 +3,10 @@
 #include <stdlib.h>
 
 void state(int q0, int q1, int i1, int i2, int* Q0, int* Q1){
-    *Q0 = i1 | q1 & (!q0) | i2 & (!q1) & q0;
-    *Q1 = i2 & (!q0) | i2 & q1 |(!i2) & (!q1) & q0 | i1 & q1 & (!q0);
+    *Q0 = i1 & (!i2)| (!i1) & q1 & (!q0) | (!i1) & i2 & (!q1) & q0;
+    *Q1 = (!i1) & (!i2) & (!q1) & q0 | (!i1) & i2 & q1 & q0 | (!i1) & i2 & (!q1) & q0 | i1 & (!i2) & q1 & (!q0);
 }
 
-void output(int q0, int q1, int* o0){
-    *o0 = q1 & q0;
+void output(int i1, int i2, int q0, int q1, int* o0){
+    *o0 = (!i1) & q1 & q0 | i1 & (!i2) & q1 & q0;
 }
