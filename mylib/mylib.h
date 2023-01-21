37c2251decdd8a84b8f83e4385a606d8c323b75c
Brigitte <114067290+brishar0n@users.noreply.github.com>
2023-01-21 20:14:26 +0700

    Update mylib.h

--- mylib/mylib.h
+++ mylib/mylib.h
@@ -1,2 +1,2 @@
-void state(int q0, int q1, int i1, int i2, int* Q0, int* Q1);
-void output(int q0, int q1, int* o0);
+void state(int i1, int i2, int q0, int q1, int* Q0, int* Q1);
+void output(int i1, int i2, int q0, int q1, int* o0);
